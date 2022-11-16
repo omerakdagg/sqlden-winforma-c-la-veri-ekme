15b91f3aa98e97376c5ee9cf3b3b82cd1c407586
omerakdagg <108828661+omerakdagg@users.noreply.github.com>
2022-11-16 17:16:12 +0300

    Proje dosyası ekle.

---
+++ WindowsFormsApp10/Form1.Designer.cs
@@ -0,0 +1,326 @@
+﻿namespace WindowsFormsApp10
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.kitap3BindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.kitaplıkDataSet = new WindowsFormsApp10.kitaplıkDataSet();
+            this.kitap3TableAdapter = new WindowsFormsApp10.kitaplıkDataSetTableAdapters.Kitap3TableAdapter();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.label9 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.kitap3BindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.kitaplıkDataSet)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(4, 288);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(784, 150);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // kitap3BindingSource
+            // 
+            this.kitap3BindingSource.DataMember = "Kitap3";
+            this.kitap3BindingSource.DataSource = this.kitaplıkDataSet;
+            // 
+            // kitaplıkDataSet
+            // 
+            this.kitaplıkDataSet.DataSetName = "kitaplıkDataSet";
+            this.kitaplıkDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // kitap3TableAdapter
+            // 
+            this.kitap3TableAdapter.ClearBeforeFill = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(93, 206);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 16);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(90, 129);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 16);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "label3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(298, 63);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(44, 16);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "label4";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(453, 42);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 9;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(453, 84);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 22);
+            this.textBox2.TabIndex = 10;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(453, 126);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 22);
+            this.textBox3.TabIndex = 11;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(453, 161);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 22);
+            this.textBox4.TabIndex = 12;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(4, 27);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 52);
+            this.button1.TabIndex = 13;
+            this.button1.Text = "veri göster";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(592, 41);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 61);
+            this.button2.TabIndex = 14;
+            this.button2.Text = "kayıt ekle";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(592, 133);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 41);
+            this.button3.TabIndex = 15;
+            this.button3.Text = "sil";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(592, 200);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 57);
+            this.button4.TabIndex = 16;
+            this.button4.Text = "güncelle";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(453, 200);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 22);
+            this.textBox5.TabIndex = 18;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(453, 234);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(100, 22);
+            this.textBox6.TabIndex = 19;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(12, 101);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 56);
+            this.button6.TabIndex = 20;
+            this.button6.Text = "max fiyat";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(12, 179);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(75, 57);
+            this.button7.TabIndex = 21;
+            this.button7.Text = "toplam kitap adedi";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(12, 245);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(75, 23);
+            this.button8.TabIndex = 22;
+            this.button8.Text = "ara";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(93, 245);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(100, 22);
+            this.textBox7.TabIndex = 23;
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(217, 60);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(75, 58);
+            this.button9.TabIndex = 24;
+            this.button9.Text = "toplam fiyatt";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(216, 142);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(75, 23);
+            this.button10.TabIndex = 25;
+            this.button10.Text = "ortalama fiyat";
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(297, 149);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(44, 16);
+            this.label9.TabIndex = 26;
+            this.label9.Text = "label9";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.kitap3BindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.kitaplıkDataSet)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private kitaplıkDataSet kitaplıkDataSet;
+        private System.Windows.Forms.BindingSource kitap3BindingSource;
+        private kitaplıkDataSetTableAdapters.Kitap3TableAdapter kitap3TableAdapter;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Label label9;
+    }
+}
+
---
+++ WindowsFormsApp10/Form1.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp10
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: Bu kod satırı 'kitaplıkDataSet.Kitap3' tablosuna veri yükler. Bunu gerektiği şekilde taşıyabilir, veya kaldırabilirsiniz.
+            this.kitap3TableAdapter.Fill(this.kitaplıkDataSet.Kitap3);
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        SqlConnection baglantim = new SqlConnection("Data Source=EXCALIBUR4;Initial Catalog=kitaplık;Integrated Security=True");
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SqlDataAdapter dk = new SqlDataAdapter("select * from Kitap3", baglantim);
+            DataSet ds = new DataSet();
+            dk.Fill(ds, "Kitap3");
+            dataGridView1.DataSource = ds.Tables["Kitap3"];
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            baglantim.Open();
+            SqlCommand kayitekle = new SqlCommand("insert into Kitap3 (kitapNo,KitapAd,KitapYazar,KitapTuru,KitapFiyat,Vergi)" +
+                "values(@k1,@k2,@k3,@k4,@k5,@k6)", baglantim);
+            kayitekle.Parameters.AddWithValue("@k1", textBox1.Text);
+            kayitekle.Parameters.AddWithValue("@k2", textBox2.Text);
+            kayitekle.Parameters.AddWithValue("@k3", textBox3.Text);
+            kayitekle.Parameters.AddWithValue("@k4", textBox4.Text);
+            kayitekle.Parameters.AddWithValue("@k5", textBox5.Text);
+            kayitekle.Parameters.AddWithValue("@k6", textBox6.Text);
+
+
+            kayitekle.ExecuteNonQuery();
+            MessageBox.Show("KAYIT EKLENDİ");
+            baglantim.Close();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            baglantim.Open();
+            SqlCommand kayitsil = new SqlCommand("delete from kitap3 where kitapAd=@ad", baglantim);
+            kayitsil.Parameters.AddWithValue("@adi", textBox1.Text);
+            //girilen kitap adini silecek.
+            kayitsil.ExecuteNonQuery();
+            MessageBox.Show("Kayit Silindi");
+            baglantim.Close();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            baglantim.Open();
+            SqlCommand kayitgüncelleme = new SqlCommand("update kitap3 set  kitapNo= @m1, kitapAd=@m2, kitapYazar=@m3, kitapTur=@m4, KitapFiyat=@m5, Vergi=@m6 where kitapNo= @m1", baglantim);
+
+            kayitgüncelleme.Parameters.AddWithValue("@m1", textBox1.Text);
+            kayitgüncelleme.Parameters.AddWithValue("@m2", textBox2.Text);
+            kayitgüncelleme.Parameters.AddWithValue("@m3", textBox3.Text);
+            kayitgüncelleme.Parameters.AddWithValue("@m4", textBox4.Text);
+            kayitgüncelleme.Parameters.AddWithValue("@m5", textBox5.Text);
+            kayitgüncelleme.Parameters.AddWithValue("@m6", textBox6.Text);
+
+            kayitgüncelleme.ExecuteNonQuery();
+            MessageBox.Show("Güncelleme Gerçekleşti");
+            baglantim.Close();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            baglantim.Open();
+            SqlCommand hesapla = new SqlCommand("Select Max(KitapFiyat) from Kitap3", baglantim);
+            SqlDataReader okuma = hesapla.ExecuteReader();
+            while (okuma.Read())
+            {
+                label3.Text = okuma[0].ToString();
+                //0.indeks=max fiyat başka bir okuma işlemi yok!
+
+            }
+            baglantim.Close();
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            baglantim.Open();
+            SqlCommand hesapla1=new SqlCommand("select count(kitapNo) from kitap3", baglantim);
+            SqlDataReader okuma1=hesapla1.ExecuteReader();
+            while (okuma1.Read())
+            {
+                label2.Text = okuma1[0].ToString();
+                //0.Indeks = count başka bir okuma işlemi yok.
+            }
+            baglantim.Close();
+        }
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+            string kitapNo = dataGridView1.Rows[e.RowIndex].Cells[0].Value.ToString();
+            string kitapAd = dataGridView1.Rows[e.RowIndex].Cells[1].Value.ToString();
+            string kitapYazar = dataGridView1.Rows[e.RowIndex].Cells[2].Value.ToString();
+            string kitapTur = dataGridView1.Rows[e.RowIndex].Cells[3].Value.ToString();
+            string kitapFiyat = dataGridView1.Rows[e.RowIndex].Cells[4].Value.ToString();
+            string Vergi = dataGridView1.Rows[e.RowIndex].Cells[5].Value.ToString();
+
+            textBox1.Text = kitapNo;
+            textBox2.Text = kitapAd;
+            textBox3.Text = kitapYazar;
+            textBox4.Text = kitapTur;
+            textBox5.Text = kitapFiyat;
+            textBox6.Text = Vergi;
+
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            SqlDataAdapter db = new SqlDataAdapter("select*from kitap3 where kitapAd='" + textBox7.Text + "'", baglantim);
+            DataSet ds = new DataSet();
+            db.Fill(ds);
+            dataGridView1.DataSource = ds.Tables[0];
+            textBox7.Clear();
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            baglantim.Open();
+            SqlCommand hesapla1= new SqlCommand("select sum(kitapFiyat) from kitap3", baglantim);
+            SqlDataReader okuma1= hesapla1.ExecuteReader();
+            while(okuma1.Read())
+            {
+                label4.Text = okuma1[0].ToString();
+            }
+            baglantim.Close();
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            baglantim.Open();
+            SqlCommand hesapla1 = new SqlCommand("select avg(kitapFiyat) from kitap3", baglantim);
+            SqlDataReader okuma1 = hesapla1.ExecuteReader();
+            while (okuma1.Read())
+            {
+                label9.Text = okuma1[0].ToString();
+            }
+            baglantim.Close();
+        }
+
+     
+
+    }
+}
